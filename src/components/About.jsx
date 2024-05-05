cb443a86f4ff0f6cb4199335be1034fde717d1eb
Ramees pk <83704042+Rameespk424@users.noreply.github.com>
2024-05-05 21:44:04 +0530

    new number

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
 import MyPic from "../assets/My-pic.png";
-import TypingAnimation from '../data/animation';
-
+import TypingAnimation from "../data/animation";
 
 const About = () => {
   return (
-    <div name="about"className="w-full h-auto min-h-screen bg-[#0a192f] flex  items-center ">
+    <div
+      name="about"
+      className="w-full h-auto min-h-screen bg-[#0a192f] flex  items-center "
+    >
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col-reverse justify-center  h-full md:flex-row mt-24">
         <div>
@@ -15,17 +17,12 @@ const About = () => {
           <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
             Ramees Pk
           </h1>
-          
-
 
           <h2 className="text-4xl sm:text-7xl mb-3 font-bold text-[#8892b0]">
             I'm a Creative Frontend Developer.
           </h2>
 
-
-        <TypingAnimation />
-
-         
+          <TypingAnimation />
 
           <button className="placeholder-gray-700 text-white group border-2 px-6 py-3 my-3 flex items-center hover:bg-pink-600 hover:border-pink-600">
             <Link to="work" smooth={true} duration={500}>
@@ -50,5 +47,3 @@ const About = () => {
 };
 
 export default About;
-
-
